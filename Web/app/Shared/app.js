cfca0b934e004ded5f738dc0a56434f7b0e216d3
Matthew Watkins <mylifegrade@gmail.com>
2014-12-27 03:31:37 +0000

    Interacting with the API and database!

--- Web/app/Shared/app.js
+++ Web/app/Shared/app.js
@@ -1,6 +1,10 @@
 (function() {
     var app = angular.module('myLifeGrade', []); 
-    app.controller('HomeController', function() {
-       this.users = []; 
-    });
+    app.controller('HomeController', [ '$http', function($http) {
+       var tmp = this;
+       tmp.users = [];
+       $http.get('../../api/users/get.php').success(function(data) {
+           tmp.users = data;
+       });
+    }]);
 })();
