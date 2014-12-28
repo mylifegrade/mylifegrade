ab3ee012456ff407698e87e6d8ee033130d5e421
Matthew Watkins <mylifegrade@gmail.com>
2014-12-28 22:43:34 +0000

    Some Ui stuff

--- Web/app/Shared/app.js
+++ Web/app/Shared/app.js
@@ -2,10 +2,10 @@
     var app = angular.module('myLifeGrade', []); 
     app.controller('HomeController', [ '$http', function($http) {
        var tmp = this;
-       tmp.users = [];
-       $http.get('../../api/current_user.php?apiKey=6ea70c49-8ed4-11e4-86fb-02d737fe62fd')
+       tmp.currentUser = {};
+       $http.get('../../api/current_user.php?apiKey=6ea70c49-8ed4-11e4-86fb-02d737fe62fd&prettyprint=true')
        .success(function(data) {
-           tmp.users = data;
+           tmp.currentUser = data;
        })
        .error(function(data){
            tmp.error = data;
