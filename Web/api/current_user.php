993e1d7b42151903477f257445cec2a1ed879e02
Matthew Watkins <mylifegrade@gmail.com>
2014-12-28 21:35:24 +0000

    Forgottne files

---
+++ Web/api/current_user.php
@@ -0,0 +1,22 @@
+<?php
+
+require 'api_base.php';
+
+class CurrentUserApiWrapper extends ApiWrapper
+{
+    public function doWork($method, $requestBody)
+    {
+        switch ($method)
+        {
+            case "GET":
+                return $this->userContext;
+            default:
+                throw new ApiException(001, "Unrecognized HTTP method: " . $method);
+        }
+    }
+}
+
+$userApi = new CurrentUserApiWrapper();
+echo $userApi->getResponseJson();
+
+?>
