a394e4b13ae0b29d21a96b4cb835d1150975c285
Matthew Watkins <mylifegrade@gmail.com>
2014-12-29 18:41:30 +0000

    Swicthed to all named parameters

--- DB/DbWrapper.php
+++ DB/DbWrapper.php
@@ -21,7 +21,7 @@ class DbWrapper
      */
     public function getUserContextByID($userID)
     {
-        return self::getUserContext("SELECT * FROM User WHERE UserID = " . $userID . " LIMIT 1;", null);
+        return self::getUserContext("SELECT * FROM User WHERE UserID = :userID LIMIT 1;", array(":userID" => $userID));
     }
     
     public function getUserContextByApiKey($apiKey)
@@ -42,7 +42,7 @@ class DbWrapper
         $user = User::createFromDbObject($selectUserResult[0]);
         
         // Populate categories and key indicators
-        $selectCategoriesResult = self::runQueryWithParameters("SELECT * FROM Category WHERE UserID = " . $user->UserID . ";", $parameters, true);
+        $selectCategoriesResult = self::runQueryWithParameters("SELECT * FROM Category WHERE UserID = :userID;", array(":userID" => $user->UserID), true);
         if (sizeof($selectCategoriesResult) > 0)
         {
             // Populate the categories and store the category IDs
