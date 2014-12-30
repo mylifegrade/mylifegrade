e9c0f97ccbd2be0f95257b4323413a785ba17a6d
Matthew Watkins <mylifegrade@gmail.com>
2014-12-30 15:28:49 +0000

    ANgluar!

--- Web/app/app.js
+++ Web/app/app.js
@@ -1,16 +1,25 @@
 (function() {
     var app = angular.module('myLifeGrade', []); 
     app.controller('HomeController', [ '$http', function($http) {
-       var tmp = this;
-       tmp.user = {};
        
-       var url = '../api/current_user.php?apiKey=329f3e41-8f9d-11e4-86fb-02d737fe62fd&prettyprint=true';
-       $http.get(url)
+       // Tabs
+       this.activeTabName = 'tabOverview';
+       this.setActiveTab = function(tabName) {
+           this.activeTabName = tabName;
+       };
+       this.isActiveTab = function(tabName) {
+           return this.activeTabName === tabName;
+       };
+       
+       // Data collection from API
+       this.user = { };
+       var tmp = this;
+       $http.get('../api/current_user.php?apiKey=329f3e41-8f9d-11e4-86fb-02d737fe62fd&prettyprint=true')
        .success(function(data) {
            tmp.user = data;
        })
        .error(function(data){
-           tmp.error = 'Problem requesting URL ' + url + ': ' + JSON.stringify(data);
+           tmp.error = JSON.stringify(data);
        });
     }]);
 })();
