35334119148f40c43b6eaf0052998d0fc2210a4e
Matthew Watkins <mylifegrade@gmail.com>
2014-12-30 00:18:08 +0000

    Much PDO about nothing

--- DB/DbWrapper.php
+++ DB/DbWrapper.php
@@ -1,6 +1,5 @@
 <?php
 
-require 'DbModels.php';
 require '../../Web/api/Models/User.php';
 
 class DbWrapper
@@ -31,7 +30,7 @@ class DbWrapper
     
     private function getUserContext($queryText, $parameters)
     {
-        $selectUserResult = self::runQueryWithParameters($queryText, $parameters, true);
+        $selectUserResult = self::runQuery($queryText, $parameters, true);
         if (sizeof($selectUserResult) == 0)
         {
             // No user found
@@ -42,7 +41,7 @@ class DbWrapper
         $user = User::createFromDbObject($selectUserResult[0]);
         
         // Populate categories and key indicators
-        $selectCategoriesResult = self::runQueryWithParameters("SELECT * FROM Category WHERE UserID = :userID;", array(":userID" => $user->UserID), true);
+        $selectCategoriesResult = self::runQuery("SELECT * FROM Category WHERE UserID = :userID;", array(":userID" => $user->UserID), true);
         if (sizeof($selectCategoriesResult) > 0)
         {
             // Populate the categories and store the category IDs
@@ -81,68 +80,97 @@ class DbWrapper
     }
     
     /*
-     * CATEGORY METHODS
+     * CATEGORY FUNCTIONS
      */
     public function addCategory($user, $category)
     {
-        /*
-        insert into Category (UserID, CategoryName, CategoryDescription, CategoryWeight, IsActive)
-        ->   values (1, 'Test Category', 'Test Description', 1, true);
-    */
+        // Insert the category
+        $queryText = "insert into Category (UserID, CategoryName, CategoryDescription, CategoryWeight, IsActive)";
+        $queryText .= " values (:userID, :categoryName, :categoryDescription, :categoryWeight, true);";
+        $parameters = array(
+            ":userID" => $user->UserID,
+            ":categoryName" => $category->CategoryID,
+            ":categoryName" => $category->CategoryName,
+            ":categoryDescription" => $category->Description,
+            ":categoryWeight" => $category->CategoryWeight
+        );
+        $category->CategoryID = self::runStatementGetLastInsertedID($queryText, $parameters);
+        
+        if (sizeof($category->KeyIndicators) > 0)
+        {
+            foreach ($category->KeyIndicators as $keyIndicator)
+            {
+                self::addKeyIndicator($category, $keyIndicator);
+            }
+        }
+        
+        // Update the object passed to us
+        $user->addCategory($category);
+        
+        // Return the user context
+        return $user;
     }
     
     
+    /*
+     * KEY INDICATOR 
+     */
+    public function addKeyIndicator($category, $keyIndicator)
+    {
+        $queryText = "insert into KeyIndicator (CategoryID, KeyIndicatorName, KeyIndicatorDescription, KeyIndicatorWeight, IsActive)";
+        $queryText .= " values (:categoryID, :keyIndicatorName, :keyIndicatorDescription, :keyIndicatorWeight, :isActive);";
+        $parameters = array(
+            ":categoryID" => $category->CategoryID,
+            ":keyIndicatorName" => $keyIndicator->KeyIndicatorName,
+            ":keyIndicatorDescription" => $keyIndicator->KeyIndicatorDescription,
+            ":keyIndicatorWeight" => $keyIndicator->KeyIndicatorWeight,
+            ":isActive" => $keyIndicator->KeyIndicatorWeight
+        );
+        $keyIndicator->KeyIndicatorID = self::runStatementGetLastInsertedID($queryText, $parameters);
+        $category->addKeyIndicator($keyIndicator);
+        return $category;
+    }
     
     /*
      * GENERIC QUERY METHODS
      */
-    public function runQueryJson($statement, $prettyPrint = false)
+    public function runQuery($queryText, $parameters = null, $fetchAsObjects = false, $className = null)
     {
-        $obj = $this->runQuery($statement, true);
-        if ($prettyPrint)
-        {
-            return json_encode($obj, JSON_PRETTY_PRINT);
-        }
-        else
-        {
-            return json_encode($obj);
-        }
-    }
-    
-    public function runQuery($queryText, $fetchAsObjects = false, $className = null)
-    {
-        return self::runQueryWithParameters($queryText, null, $fetchAsObjects, $className);
-    }
-    
-    public function runQueryWithParameters($queryText, $parameters = null, $fetchAsObjects = false, $className = null)
-    {
-        $dbConn = self::createDbConnection();
-        $statement = $dbConn->prepare($queryText);
+        $statement = self::createAndExecuteStatement($queryText, $parameters);
         
-        // Logging
-        // echo "Running " . $statement->queryString;
-
-        // Execute the statement
-        $statement->execute($parameters);
-        
+        // Return the query results
         if ($fetchAsObjects)
         {
             if ($className != null)
             {
-                // Return known object type
+                // Return as a known types
                 return $statement->fetchAll(PDO::FETCH_CLASS, $className);
             }
             
-            // Return anonymous objects
+            // Return as anonymous objects
             return $statement->fetchAll(PDO::FETCH_OBJ);
         }
         
-        // Return associative array
+        // Return as an associative array
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
     
-    private function createDbConnection()
+    public function runStatementGetLastInsertedID($queryText, $parameters = null)
     {
+        $statement = self::createAndExecuteStatement($queryText, $parameters);
+        // TODO: Get the last inserted ID
+    }
+    
+    public function runStatementGetAffectedRows($queryText, $parameters = null)
+    {
+        $statement = self::createAndExecuteStatement($queryText, $parameters);
+        // TODO: Get the count of affected rows
+    }
+    
+    private function createAndExecuteStatement($queryText, $parameters = null)
+    {
+        // echo "Preparing to run " . $queryText;
+        
         $dbConn = null;
         try
         {
@@ -157,7 +185,17 @@ class DbWrapper
             die("Error while initializing the DB connection: " . $e->getMessage());
         }
         
-        return $dbConn;
+        // Create the statement
+        $statement = $dbConn->prepare($queryText);
+        
+        // Logging
+        // echo "Running " . $statement->queryString;
+
+        // Execute the statement
+        $statement->execute($parameters);
+        
+        // Return the statement
+        return $statement;
     }
 }
 
--- Web/app/Shared/app.js
+++ Web/app/Shared/app.js
@@ -3,7 +3,7 @@
     app.controller('HomeController', [ '$http', function($http) {
        var tmp = this;
        tmp.currentUser = {};
-       $http.get('../../api/current_user.php?apiKey=6ea70c49-8ed4-11e4-86fb-02d737fe62fd&prettyprint=true')
+       $http.get('../../api/current_user.php?apiKey=329f3e41-8f9d-11e4-86fb-02d737fe62fd&prettyprint=true')
        .success(function(data) {
            tmp.currentUser = data;
        })
