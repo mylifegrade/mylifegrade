316bb326b5d00826f56095edd15e1e87f46c5749
Matthew Watkins <mylifegrade@gmail.com>
2015-01-01 17:11:49 +0000

    Broke out UserController

--- Web/app/app.js
+++ Web/app/app.js
@@ -1,9 +1,9 @@
 (function() {
     var app = angular.module('myLifeGrade', []); 
-    app.controller('HomeController', [ '$http', function($http) {
-       
+    
+    app.controller('HomeController', function() {
+        
         // Init
-        this.user = { };
         this.activeTabName = 'tabOverview';
         
         // Tabs
@@ -13,6 +13,8 @@
         this.isActiveTab = function(tabName) {
             return this.activeTabName === tabName;
         };
+        
+        // Serialization
         this.serialize = function(obj, prettyPrint, splitIntoLines) {
             if (prettyPrint) {
                 var json = JSON.stringify(obj, undefined, 4);
@@ -23,8 +25,14 @@
             }
             return JSON.stringify(obj);
         };
+    });
+    
+    app.controller('UserController', [ '$http', function($http) {
+       
+        // Init
+        this.user = { };
         
-            // Data collection from API
+        // Data collection from API
         var tmp = this;
         $http.get('../api/current_user.php?apiKey=329f3e41-8f9d-11e4-86fb-02d737fe62fd&prettyprint=true')
         .success(function(data) {
