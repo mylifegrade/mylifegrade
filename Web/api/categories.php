827e6926e1ee4060f2315c403e633dfb72d31998
Matthew Watkins <mylifegrade@gmail.com>
2015-01-03 02:01:21 +0000

    Progress

--- Web/api/categories.php
+++ Web/api/categories.php
@@ -9,7 +9,18 @@ class CategoriesApiWrapper extends ApiWrapper
         switch ($method)
         {
             case "POST":
-                return "Well, hi!";
+                $categoryPostDataString = file_get_contents('php://input');
+                if (strlen($categoryPostDataString) > 0)
+                {
+                    $category = json_decode($categoryPostDataString);
+                    if ($category == null)
+                    {
+                        throw new ApiException(123, "Bad JSon data");
+                    }
+                    
+                    $this->db->addCategory($this->userContext, $category);
+                    return $this->userContext;
+                }
             default:
                 throw new ApiException(001, "Unrecognized HTTP method: " . $method);
         }
