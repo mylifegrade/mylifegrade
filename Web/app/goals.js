7fa6040ab8cedd2652b9c42abfad1b0e8686cb4a
Matthew Watkins <mylifegrade@gmail.com>
2015-01-13 19:18:46 +0000

    Moved key indicator controlelr logic to category

--- Web/app/goals.js
+++ Web/app/goals.js
@@ -8,6 +8,7 @@
         // Init
         this.selectedCategory = { CategoryID: -1 };
         this.categoryToAdd = { CategoryWeight: 3 };
+        this.keyIndicatorToAdd = { KeyIndicatorWeight: 3 };
         
         this.isSelectedOrDefault = function(category) {
             if (this.selectedCategory.CategoryID == -1)
@@ -38,6 +39,23 @@
                alert("ERROR:" + JSON.stringify(data));
             });
         };
+        
+        // Functions
+        this.addKeyIndicator = function (category) {
+            // Temporary callback variables
+            var thisCtrl = this;
+            var thisCategory = category;
+            // var thisUser = user;
+            
+            // Issue the post
+            $http.post('../api/key_indicators.php?apiKey=53288076-3666-48e6-a978-27a6c5962ad3&categoryID=' + thisCategory.CategoryID + '&prettyprint=true', thisCtrl.keyIndicatorToAdd)
+            .success(function(data) {
+               thisCategory.KeyIndicators = data.KeyIndicators;
+            })
+            .error(function(data) {
+               alert("ERROR:" + JSON.stringify(data));
+            });
+        };
     }]);
     
     app.directive('goals', function() {
@@ -58,24 +76,7 @@
      * KEY INDICATORS
      */
     app.controller('KeyIndicatorController', ['$http', function($http) {
-        // Init
-        this.keyIndicatorToAdd = { KeyIndicatorWeight: 3 };
         
-        // Functions
-        this.addKeyIndicator = function (category) {
-            // Temporary callback variables
-            var thisCtrl = this;
-            var thisCategory = category;
-            
-            // Issue the post
-            $http.post('../api/key_indicators.php?apiKey=53288076-3666-48e6-a978-27a6c5962ad3&categoryID=' + thisCategory.CategoryID + '&prettyprint=true', thisCtrl.keyIndicatorToAdd)
-            .success(function(data) {
-               thisCategory.KeyIndicators = data.KeyIndicators;
-            })
-            .error(function(data) {
-               alert("ERROR:" + JSON.stringify(data));
-            });
-        };
     }]);
     
     app.directive('addKeyIndicator', function() {
